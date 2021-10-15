32ce45d0949f27fc2a3e52945f7dee0339baf512
Evgeniy Burdin <e.s.burdin@gmail.com>
2021-10-15 20:54:57 +0400

    0.1.1

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup
 
 setup(
     name='logdec',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     author='Evgeniy Burdin',
     author_email='e.s.burdin@gmail.com',
     packages=['logdec'],
-    description='Decorator for logging exceptions in functions and methods.',
+    description='Decorator for logging functions and methods.',
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     long_description_content_type="text/markdown",
-    url='https://github.com/EvgeniyBurdin/logexdec',
+    url='https://github.com/EvgeniyBurdin/logdec',
     keywords='decorator function method logging logger exception',
     classifiers=[
         'Programming Language :: Python :: 3.7',
