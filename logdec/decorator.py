33296328b0244ea36c8199d04079cc505c6307e4
Evgeniy Burdin <e.s.burdin@gmail.com>
2021-10-19 17:21:39 +0400

    Сделал новый декоратор

--- logdec/decorator.py
+++ logdec/decorator.py
@@ -1,41 +1,43 @@
 import asyncio
 import functools
-from typing import Any, Tuple, Union
+from typing import Callable, Optional, Tuple, Union
 
-from .settings import FuncInfo, LogDec
+from .data_classes import CallInfo, FuncInfo
+from .handlers import Handler
 
-log_dec = LogDec()
+handler = Handler()
 
 
 def logex(
     *names_or_func,
-    reraise: Union[Tuple[Exception, ...], Exception] = (),
-    return_value: Any = None,  # return value for no reraise
-    exc_info: bool = False,    # whether to include traceback in the log
-    log_dec: LogDec = log_dec,
+    no_reraise: Union[Tuple[Exception, ...], Exception] = (),
+    callback: Optional[Callable] = None,  # for no_reraise
+    handler: Handler = handler,
 ):
     def _decor(func):
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
 
-            func_info = FuncInfo(
-                func=func, args=args, kwargs=kwargs,
-                reraise=reraise, return_value=return_value, exc_info=exc_info,
-            )
+            def make_call_info(exception: Exception) -> CallInfo:
+                func_info = FuncInfo(func=func, args=args, kwargs=kwargs)
+                return CallInfo(
+                    func_info=func_info, no_reraise=no_reraise,
+                    callback=callback
+                )
 
             if asyncio.iscoroutinefunction(func):
                 async def async_func():
                     try:
                         return await func(*args, **kwargs)
-                    except Exception as exc:
-                        return log_dec.handling_exception(func_info, exc)
+                    except Exception as exception:
+                        return handler.exception(make_call_info(exception))
                 return async_func()
             else:
                 try:
                     return func(*args, **kwargs)
-                except Exception as exc:
-                    return log_dec.handling_exception(func_info, exc)
+                except Exception as exception:
+                    return handler.exception(make_call_info(exception))
 
         return wrapper
 
