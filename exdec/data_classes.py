4dd2f08d566dfee80c8447747fe9dec51dc92f3f
Evgeniy Burdin <e.s.burdin@gmail.com>
2021-10-21 16:16:26 +0400

    Добавил в декоратор заготовки для пре и пост обработки

--- exdec/data_classes.py
+++ exdec/data_classes.py
@@ -7,12 +7,14 @@ class FuncInfo:
     func: Callable
     args: Tuple[Any, ...]
     kwargs: Dict[str, Any]
-    exception: Exception
+    exception: Optional[Exception] = None
 
 
 @dataclass
 class DecData:
     exceptions: Union[Tuple[Type[Exception], ...], Type[Exception]]
     exclude: bool
-    handler: Optional[Callable[[FuncInfo], Any]]
+    before_handler: Optional[Callable[[FuncInfo], Any]]
+    after_handler: Optional[Callable[[FuncInfo], Any]]
+    exc_handler: Optional[Callable[[FuncInfo], Any]]
     func_info: FuncInfo
--- exdec/decorator.py
+++ exdec/decorator.py
@@ -11,42 +11,47 @@ catcher = Catcher()
 def catch(
     *dec_args,
     exclude: bool = False,
-    handler: Optional[Callable[[FuncInfo], Any]] = None,
+    before_handler: Optional[Callable[[FuncInfo], Any]] = None,
+    after_handler: Optional[Callable[[FuncInfo], Any]] = None,
+    exc_handler: Optional[Callable[[FuncInfo], Any]] = None,
     catcher: Catcher = catcher,
 ):
     exceptions = catcher.make_exceptions(dec_args)
 
-    def _decor(func):
+    def decor(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
 
-            def make_dec_data(exception: Exception) -> DecData:
-                return DecData(
-                    exceptions=exceptions, exclude=exclude, handler=handler,
-                    func_info=FuncInfo(
-                        func=func, args=args, kwargs=kwargs,
-                        exception=exception,
-                    ),
-                )
+            dec_data = DecData(
+                exceptions=exceptions,
+                exclude=exclude,
+                before_handler=before_handler,
+                after_handler=after_handler,
+                exc_handler=exc_handler,
+                func_info=FuncInfo(func=func, args=args, kwargs=kwargs),
+            )
 
             if asyncio.iscoroutinefunction(func):
                 async def async_func():
                     try:
-                        return await func(*args, **kwargs)
+                        result = await func(*args, **kwargs)
                     except Exception as exception:
-                        return await catcher.aio_handle_exception(
-                            make_dec_data(exception)
-                        )
-                return async_func()
+                        dec_data.func_info.exception = exception
+                        result = await catcher.aio_handle_exception(dec_data)
+                    return result
+                wrapper_result = async_func()
             else:
                 try:
-                    return func(*args, **kwargs)
+                    wrapper_result = func(*args, **kwargs)
                 except Exception as exception:
-                    return catcher.handle_exception(make_dec_data(exception))
+                    dec_data.func_info.exception = exception
+                    wrapper_result = catcher.handle_exception(dec_data)
+
+            return wrapper_result
 
         return wrapper
 
     if dec_args and callable(dec_args[0]) and not type(dec_args[0]) is type:
-        return _decor(dec_args[0])
+        return decor(dec_args[0])
     else:
-        return _decor
+        return decor
