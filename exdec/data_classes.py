2cf3dd2fac7861713a31bc5746fd7c26939cb83d
Evgeniy Burdin <e.s.burdin@gmail.com>
2021-10-21 20:54:28 +0400

    Добавил докстринги

--- exdec/data_classes.py
+++ exdec/data_classes.py
@@ -1,9 +1,17 @@
+""" Module with used data classes.
+"""
 from dataclasses import dataclass
-from typing import Any, Callable, Dict, Optional, Tuple, Type, Union
+from typing import Any, Callable, Dict, Optional, Tuple, Type
 
 
 @dataclass
 class FuncInfo:
+    """ Decorated function information.
+
+    If an exception occurs during the execution of the function, it will be
+    stored in `exception`. This information will be available in the exception
+    handler.
+    """
     func: Callable
     args: Tuple[Any, ...]
     kwargs: Dict[str, Any]
@@ -12,6 +20,11 @@ class FuncInfo:
 
 @dataclass
 class DecData:
-    exceptions: Union[Tuple[Type[Exception], ...], Type[Exception]]
+    """ Data available inside the decorator.
+
+    If `exclude` set to `True`, then the decorator will not catch `exceptions`.
+    If in `False`, then the decorator will catch only `exceptions`.
+    """
+    exceptions: Tuple[Type[Exception], ...]
     exclude: bool
     func_info: FuncInfo
