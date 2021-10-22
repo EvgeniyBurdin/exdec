14c9a5a42bbb715bcd8839b69bde41072fcad6f0
Evgeniy Burdin <e.s.burdin@gmail.com>
2021-10-22 11:09:21 +0400

    0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='exdec',
-    version='0.2.0',
+    version='0.3.0',
     license='MIT',
     author='Evgeniy Burdin',
     author_email='e.s.burdin@gmail.com',
