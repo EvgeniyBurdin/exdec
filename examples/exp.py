7f388ddb8272ae9a6ede96469b18d56f66611043
Evgeniy Burdin <e.s.burdin@gmail.com>
2021-10-22 11:07:41 +0400

    Сделал пример для быстрого старта

--- examples/exp.py
+++
@@ -1,8 +0,0 @@
-from examples.simple import SomeClass
-import asyncio
-
-
-c = SomeClass()
-
-
-print(asyncio.run(c.bar(1)))
---
+++ examples/quick_start.py
@@ -0,0 +1,76 @@
+from exdec.data_classes import FuncInfo
+from exdec.decorator import catch
+
+
+# 1 --------------------------------------------------------------------------
+
+# Catching all exceptions
+@catch
+def safe_div_1(x: int, y: int) -> float:
+    result = x / y
+    return result
+
+
+z = safe_div_1(3, 3)
+assert z == 1.0
+z = safe_div_1(3, 0)
+assert z is None
+
+
+# 2 --------------------------------------------------------------------------
+
+# Catching only ZeroDivisionError
+@catch(ZeroDivisionError)
+def safe_div_2(x: int, y: int) -> float:
+    result = x / y
+    return result
+
+
+z = safe_div_2(3, 0)
+assert z is None
+
+
+# 3 --------------------------------------------------------------------------
+
+NEW_RESULT = 0.01
+
+
+def exc_handler(func_info: FuncInfo) -> float:
+    exc = func_info.exception
+    print(f"Caught an exception {type(exc)}: {exc}.")
+    fname = func_info.func.__name__
+    args = func_info.args
+    print(f"Result {fname}{args} changed to {NEW_RESULT}")
+    return NEW_RESULT
+
+
+# Catching only ZeroDivisionError
+@catch(ZeroDivisionError, exc_handler=exc_handler)
+def safe_div_3(x: int, y: int) -> float:
+    result = x / y
+    return result
+
+
+z = safe_div_3(3, 0)
+assert z == NEW_RESULT
+
+
+# 4 --------------------------------------------------------------------------
+
+class MyException_1(Exception):
+    pass
+
+
+class MyException_2(Exception):
+    pass
+
+
+# Catching all exceptions except for (MyException_1, MyException_2)
+@catch(MyException_1, MyException_2, exclude=True, exc_handler=exc_handler)
+def safe_div_4(x: int, y: int) -> float:
+    result = x / y
+    return result
+
+
+z = safe_div_4(3, 0)
+assert z == NEW_RESULT
