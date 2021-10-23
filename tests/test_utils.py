28e824bb6feb3d2fe8f2fdf2114106f2a7564e9e
Evgeniy Burdin <e.s.burdin@gmail.com>
2021-10-23 17:40:48 +0400

    Добавил аннотации

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,12 @@
+from typing import Any, Callable
+
 import pytest
-from exdec.data_classes import DecData
+from exdec.data_classes import DecData, FuncInfo
 from exdec.utils import (ExDecException, check_exception_class, check_handler,
                          try_reraise)
 
 
-def test_try_reraise_success(dec_data: DecData, custom_exception):
+def test_try_reraise_success(dec_data: DecData, custom_exception: Exception):
 
     dec_data.func_info.exception = Exception("message")
     dec_data.exclude = False
@@ -23,7 +25,7 @@ def test_try_reraise_success(dec_data: DecData, custom_exception):
         try_reraise(dec_data)
 
 
-def test_try_reraise_fail(dec_data: DecData, custom_exception):
+def test_try_reraise_fail(dec_data: DecData, custom_exception: Exception):
 
     dec_data.func_info.exception = custom_exception
     dec_data.exclude = False
@@ -36,12 +38,12 @@ def test_try_reraise_fail(dec_data: DecData, custom_exception):
     try_reraise(dec_data)
 
 
-def test_check_handler_success(handler):
+def test_check_handler_success(handler: Callable[[FuncInfo], Any]):
 
     check_handler(handler)
 
 
-def test_check_handler_fail(func):
+def test_check_handler_fail(func: Callable):
 
     with pytest.raises(ExDecException):
         check_handler(1)  # Handler not callable
@@ -51,7 +53,7 @@ def test_check_handler_fail(func):
         check_handler(func)
 
 
-def test_check_exception_class_success(custom_exception):
+def test_check_exception_class_success(custom_exception: Exception):
 
     check_exception_class(type(custom_exception))
 
