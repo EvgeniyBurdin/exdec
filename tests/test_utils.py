a4a2c7ce5acc5e455eeb65b1e976ca8faa53de89
Evgeniy Burdin <e.s.burdin@gmail.com>
2021-10-23 16:35:44 +0400

    Добавил комменты к тесту

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,12 +9,16 @@ def test_try_reraise_successfully(dec_data: DecData, custom_exception):
     dec_data.func_info.exception = Exception("message")
     dec_data.exclude = False
     dec_data.exceptions = (type(custom_exception), )
+    # Exception occurred not from `dec_data.exceptions` tuple, and they
+    # are reraised because `exclude` is False
     with pytest.raises(Exception):
         try_reraise(dec_data)
 
     dec_data.func_info.exception = custom_exception
     dec_data.exclude = True
     dec_data.exceptions = (Exception, )
+    # Exception occurred from the `dec_data.exceptions` tuple, but they are
+    # being reraised because `exclude` is True
     with pytest.raises(type(custom_exception)):
         try_reraise(dec_data)
 
