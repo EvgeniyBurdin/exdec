c5688a48f3da48d5450faad675aa04c67cbb95b6
Evgeniy Burdin <e.s.burdin@gmail.com>
2021-10-24 21:15:29 +0400

    Добавил тест декоратора для запускателей обработчиков в менеджере

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,24 +1,43 @@
+import asyncio
+from typing import Any, Callable
+
+from exdec.data_classes import DecData, FuncInfo
+from exdec.manager import Manager, execute_wrapper
 from exdec.utils import (check_exception_class, check_handler,
                          default_exc_handler, try_reraise)
 
-from exdec.data_classes import FuncInfo
-from exdec.manager import Manager
+
+class FakeManager:
+
+    is_try_reraise_called = False
+
+    def try_reraise(self, func_info: FuncInfo):
+        self.is_try_reraise_called = True
+
+    def execute_handler(self, handler: Callable, dec_data: DecData):
+        return handler(dec_data.func_info)
+
+    async def async_execute_handler(
+        self, handler: Callable, dec_data: DecData
+    ):
+        return handler(dec_data.func_info)
 
 
-def any_handler(func_info: FuncInfo):
-    pass
+def test_execute_wrapper(
+    handler: Callable[[FuncInfo], Any], dec_data: DecData
+):
+    fm = FakeManager()
 
+    _execute = execute_wrapper(FakeManager.execute_handler)
+    result = _execute(fm, handler, dec_data)
+    assert not fm.is_try_reraise_called
+    assert result is None
 
-def any_func():
-    pass
-
-
-class AnyException_1(Exception):
-    pass
-
-
-class AnyException_2(Exception):
-    pass
+    dec_data.func_info.exception = Exception()
+    _execute = execute_wrapper(FakeManager.async_execute_handler)
+    result = asyncio.run(_execute(fm, handler, dec_data))
+    assert fm.is_try_reraise_called
+    assert result is None
 
 
 def test_init(manager: Manager):
@@ -30,13 +49,13 @@ def test_init(manager: Manager):
     assert manager.default_exception_classes == (Exception, )
 
 
-def test_make_handlers(manager: Manager):
+def test_make_handlers(manager: Manager, handler: Callable[[FuncInfo], Any]):
 
     before_handler, after_handler, exc_handler = manager.make_handlers(
-        any_handler, any_handler, any_handler
+        handler, handler, handler
     )
-    for handler in (before_handler, after_handler, exc_handler):
-        assert handler == any_handler
+    for h in (before_handler, after_handler, exc_handler):
+        assert h == handler
 
     before_handler, after_handler, exc_handler = manager.make_handlers(
         None, None, None
@@ -46,7 +65,15 @@ def test_make_handlers(manager: Manager):
     assert exc_handler == manager.exc_handler
 
 
-def test_make_exceptions(manager: Manager):
+class AnyException_1(Exception):
+    pass
+
+
+class AnyException_2(Exception):
+    pass
+
+
+def test_make_exceptions(manager: Manager, func: Callable):
 
     exceptions = manager.make_exceptions(dec_args=tuple())
     assert exceptions == manager.default_exception_classes
@@ -55,7 +82,7 @@ def test_make_exceptions(manager: Manager):
     # def any_func():
     #     ...
     # This decorator will make dec_args==(any_func, )
-    exceptions = manager.make_exceptions(dec_args=(any_func, ))
+    exceptions = manager.make_exceptions(dec_args=(func, ))
     assert exceptions == manager.default_exception_classes
 
     exceptions = manager.make_exceptions(
