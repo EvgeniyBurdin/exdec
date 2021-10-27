7c36af75200414703781b10daacbf8e0d08b2a92
Evgeniy Burdin <e.s.burdin@gmail.com>
2021-10-27 12:47:55 +0400

    Поправил пример

--- examples/simple_extra_usage.py
+++ examples/simple_extra_usage.py
@@ -2,9 +2,12 @@ from exdec.data_classes import FuncInfo
 from exdec.decorator import catch
 
 
+_EXTRA_RU = "ru"
+
+
 def exc_handler(func_info: FuncInfo) -> float:
 
-    if func_info.extra == "ru":
+    if func_info.extra == _EXTRA_RU:
         message = "Поймано исключение!"
     else:
         message = "Caught an exception!"
@@ -15,7 +18,7 @@ def exc_handler(func_info: FuncInfo) -> float:
 
 
 # Catching only ZeroDivisionError and reraise
-@catch(ZeroDivisionError, exc_handler=exc_handler, extra="ru")
+@catch(ZeroDivisionError, exc_handler=exc_handler, extra=_EXTRA_RU)
 def div(x: int, y: int) -> float:
     return x / y
 
